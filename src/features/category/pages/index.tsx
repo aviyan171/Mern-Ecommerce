06ea14b8635ad0d2bf9ea73fd0cebf1002ef184f
Abhiyan Upreti <abhiyan@noveltytechnology.com>
2024-01-21 14:10:04 +0545

    category page

--- src/features/category/pages/index.tsx
+++ src/features/category/pages/index.tsx
@@ -1,4 +1,4 @@
-import CurrentPagesIndicator from '../../../shared/components/CurrentPagesIndicator';
+import CurrentPagesIndicator from 'shared/components/CurrentPagesIndicator';
 import CategoryDetails from '../components/CategoryDetails';
 
 function Category() {
