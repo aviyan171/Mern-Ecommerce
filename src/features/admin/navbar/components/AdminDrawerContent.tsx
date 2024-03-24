00f3d72241ba285b57752518f7f6dcdfccc370f0
Abhiyan Upreti <abhiyan@noveltytechnology.com>
2024-03-24 12:25:49 +0545

    Added current route

--- src/features/admin/navbar/components/AdminDrawerContent.tsx
+++ src/features/admin/navbar/components/AdminDrawerContent.tsx
@@ -1,7 +1,9 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { adminRoutes } from 'shared/constants';
 
 export function AdminDrawerContent({ onlyLogo }: { onlyLogo: boolean }) {
+  const location = useLocation();
+  const isSameRoute = (route: string) => route === location.pathname;
   return (
     <div className="overflow-hidden">
       <div className="mt-10 flex items-center">
@@ -19,7 +21,11 @@ export function AdminDrawerContent({ onlyLogo }: { onlyLogo: boolean }) {
               key={i.name}
               className={`flex items-center ${
                 !onlyLogo ? 'justify-center ' : 'justify-start'
-              }cursor-pointer mt-2 gap-2  ${onlyLogo ? 'pl-3' : 'pl-0'}`}
+              }cursor-pointer mt-2 gap-2  ${
+                onlyLogo ? 'pl-3' : 'pl-0'
+              } hover:bg-slate-200 ${
+                isSameRoute(i.link) ? 'bg-slate-200' : ''
+              }`}
             >
               <img src={i.img} className="h-[40px] w-[40px]" />
               {onlyLogo && <p>{i.name}</p>}
