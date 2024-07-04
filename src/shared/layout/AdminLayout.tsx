3148f3b8c4d4b6337da7f9cc77ff44a30f5cf752
abhiyanNovelty1 <abhiyan@noveltytechnology.com>
2024-07-04 18:20:06 +0545

    fixed some css

--- src/shared/layout/AdminLayout.tsx
+++ src/shared/layout/AdminLayout.tsx
@@ -1,40 +1,31 @@
-import { AdminDrawerContent } from 'features/admin/navbar/components/AdminDrawerContent';
-import AdminNavbar from 'features/admin/navbar/components/AdminNavbar';
-import { useState } from 'react';
-import { Outlet } from 'react-router-dom';
-import Drawer from 'shared/components/Drawer';
+import { AdminDrawerContent } from 'features/admin/navbar/components/AdminDrawerContent'
+import AdminNavbar from 'features/admin/navbar/components/AdminNavbar'
+import { useState } from 'react'
+import { Outlet } from 'react-router-dom'
+import Drawer from 'shared/components/Drawer'
 
 export const AdminLayout = () => {
-  const [collapsible, setCollapsible] = useState(true);
+  const [collapsible, setCollapsible] = useState(true)
 
   const calculateCollapsible = () => {
-    return collapsible
-      ? `fixed w-[calc(100%-75px-28px)] top-0`
-      : `fixed w-[calc(100%-280px-28px)] top-0`;
-  };
+    return collapsible ? `fixed w-[calc(100%-75px-28px)] top-0 z-[1] ` : `fixed w-[calc(100%-280px-28px)] top-0 z-[1]`
+  }
   return (
     <div>
-      <Drawer
-        collapsible={collapsible}
-        handleCollapsible={() => setCollapsible(!collapsible)}
-      >
+      <Drawer collapsible={collapsible} handleCollapsible={() => setCollapsible(!collapsible)}>
         <AdminDrawerContent onlyLogo={!collapsible} />
       </Drawer>
+
       <div
-        className={` ${
-          collapsible ? `ml-COLLAPSIBLE_DRAWER_WIDTH` : `ml-DRAWER_WIDTH`
-        } transition-[margin-left] duration-500 ease-in-out`}
+        className={` ${collapsible ? `ml-COLLAPSIBLE_DRAWER_WIDTH` : `ml-DRAWER_WIDTH`} transition-[margin-left] duration-500 ease-in-out`}
       >
         <div className="px-7 py-4">
-          <AdminNavbar
-            collapsibleWidth={calculateCollapsible()}
-            key={calculateCollapsible()}
-          />
+          <AdminNavbar collapsibleWidth={calculateCollapsible()} key={calculateCollapsible()} />
         </div>
         <div className="px-4">
           <Outlet />
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
