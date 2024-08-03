8a03912ad70ca740ce0e1c99c6eb8a70067f2aab
abhiyanNovelty1 <abhiyan@noveltytechnology.com>
2024-08-03 18:28:19 +0545

    fixed issue

--- src/shared/components/ProductListItems.tsx
+++ src/shared/components/ProductListItems.tsx
@@ -17,27 +17,23 @@ function ProductListItems({ image, name, price, productId }: ProductProps) {
   }
 
   return (
-    <div className="group cursor-pointer" onClick={handleOnClick}>
-      <div className="h-[256px] bg-no-repeat bg-contain relative" style={{ backgroundImage: `url(${image})` }}>
+    <div className="group ">
+      <div className="relative">
+        <img className="h-[256px] object-fill  cursor-pointer w-full" src={image} onClick={handleOnClick} />
         <div className="absolute px-4 py-1 bg-black  text-white text-xs top-5 left-0">SALE</div>
         <div
           className="absolute 
                  flex flex-col gap-3
                 top-5 right-0 group-hover:right-6
                 invisible group-hover:visible  
-                 transition-[right] duration-700 ease-in-out  "
+                 transition-[right] duration-700 ease-in-out cursor-pointer "
         >
           <div className="bg-white p-2">
             <BsHeart />
           </div>
-          {/* <div className="bg-white p-2">
-            <BsArrowLeftRight />
-          </div>
-          <div className="bg-white p-2">
-            <BsSearch />
-          </div> */}
         </div>
       </div>
+
       <div className="mt-[25px] flex flex-col gap-1">
         <p>{name}</p>
         <p className="group-hover:text-red-500 cursor-pointer transition-all">+ Add To Cart</p>
